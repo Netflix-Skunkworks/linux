4eaab56f4c2d922eb99536028df9754f0f3fc6da
Sargun Dhillon <sargun@sargun.me>
2018-10-11 23:28:57 -0700

    Gate fsync by cap_sys_admin
    Anyone who doesn't have cap_sys_admin has no business
    stalling out the entire system's I/O.

--- fs/sync.c
+++ fs/sync.c
@@ -192,6 +192,9 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 
 	if (!file->f_op->fsync)
 		return -EINVAL;
+	/* Only SYS_ADMIN has any duty telling us to sync */
+	if (!capable(CAP_SYS_ADMIN))
+		return 0;
 	if (!datasync && (inode->i_state & I_DIRTY_TIME))
 		mark_inode_dirty_sync(inode);
 	return file->f_op->fsync(file, start, end, datasync);
