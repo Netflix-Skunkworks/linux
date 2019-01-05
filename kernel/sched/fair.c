1f0697590ff9e56e03e5b88b8f7150d5949452f1
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2019-01-04 18:05:43 -0800

    Fix locking in unregister_fair_sched_group

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10010,17 +10010,12 @@ void unregister_fair_sched_group(struct task_group *tg)
 		if (tg->se[cpu])
 			remove_entity_load_avg(tg->se[cpu]);
 
-		/*
-		 * Only empty task groups can be destroyed; so we can speculatively
-		 * check on_list without danger of it being re-added.
-		 */
-		if (!tg->cfs_rq[cpu]->on_list)
-			continue;
-
 		rq = cpu_rq(cpu);
-
 		raw_spin_lock_irqsave(&rq->lock, flags);
-		list_del_leaf_cfs_rq(tg->cfs_rq[cpu]);
+
+		if (tg->cfs_rq[cpu]->on_list)
+		    list_del_leaf_cfs_rq(tg->cfs_rq[cpu]);
+
 		raw_spin_unlock_irqrestore(&rq->lock, flags);
 	}
 }
