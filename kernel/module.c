03fb66080db2a24267567279894026d9433e0148
Sargun Dhillon <sargun@sargun.me>
2020-11-16 21:03:43 -0800

    Patch out nvidia module tainting behaviour

--- kernel/module.c
+++ kernel/module.c
@@ -1437,6 +1437,7 @@ static int verify_namespace_is_imported(const struct load_info *info,
 	return 0;
 }
 
+#if 0
 static bool inherit_taint(struct module *mod, struct module *owner)
 {
 	if (!owner || !test_bit(TAINT_PROPRIETARY_MODULE, &owner->taints))
@@ -1455,6 +1456,7 @@ static bool inherit_taint(struct module *mod, struct module *owner)
 	}
 	return true;
 }
+#endif
 
 /* Resolve a symbol for this module.  I.e. if we find one, record usage. */
 static const struct kernel_symbol *resolve_symbol(struct module *mod,
@@ -1480,6 +1482,7 @@ static const struct kernel_symbol *resolve_symbol(struct module *mod,
 	if (!sym)
 		goto unlock;
 
+#if 0
 	if (license == GPL_ONLY)
 		mod->using_gplonly_symbols = true;
 
@@ -1487,6 +1490,7 @@ static const struct kernel_symbol *resolve_symbol(struct module *mod,
 		sym = NULL;
 		goto getname;
 	}
+#endif
 
 	if (!check_version(info, name, mod, crc)) {
 		sym = ERR_PTR(-EINVAL);
